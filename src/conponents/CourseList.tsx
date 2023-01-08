3e07c163773bc5c1ef3836e0c9b8b91368eeafa7
ParkDongJo <pdj10211@gmail.com>
2023-01-09 00:55:38 +0900

    Feat: Implement the Course components(List, Item)

--- src/conponents/CourseList.tsx
+++ src/conponents/CourseList.tsx
@@ -4,23 +4,28 @@ import CourseRow, { CourseData, Step } from './CourseRow'
 
 const CourseList: React.FC<Props> = (props) => {
     const { datas } = props
-    const [selectedRowId, setSelectedRowId] = useState<number>()
-    const onClickRow = (id: number) => {
+    const [selectedRowId, setSelectedRowId] = useState<string>()
+
+    const onClickRow = (id: string) => {
+        if (selectedRowId) {
+            setSelectedRowId(undefined)
+            return
+        }
         setSelectedRowId(id)
     }
 
     return (
         <Container>
             {datas.map(({ id, iconUrl, title, desc, ...rest }: CourseData) => (
-                <Row>
-                    <Head folded={selectedRowId === id} onClick={() => { onClickRow(id) }}>
+                <Row key={id}>
+                    <Head folded={false} onClick={() => { onClickRow(id) }}>
                         <img src={iconUrl} alt='' />
                         <div>
                             <h5>{title}</h5>
                             <Description>{desc}</Description>
                         </div>
                     </Head>
-                    <CourseRow visiable={selectedRowId === id} {...rest} />
+                    <CourseRow visiable={!!selectedRowId && selectedRowId === id} {...rest} />
                 </Row>
             ))}
         </Container>
@@ -33,23 +38,43 @@ type Props = {
 }
 
 const Container = styled.div`
-    
 `
 const Row = styled.div`
-    
 `
 const Head = styled.div<{folded: boolean}>`
+    max-width: 640px;
     font-size: 1em;
-    padding: 10px 15px;
+    padding: 30px 20px 20px 20px;
+    display: flex;
+    flex-direction: row;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.5);
     & img {
         width: 60px;
         height: 60px;
     }
     & div {
-        
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        text-align: left;
+        padding-left: 20px;
+        & h5, p {
+            margin: 4px 0 !important;
+        }
+        & h5 {
+            font-family: 'NanumSquareNeoExtraBold';
+            font-size: 1.3em;
+        }
+        & p {
+            font-family: 'NanumSquareNeo';
+            font-size: 1em;
+        }
+    }
+    &:hover {
+        cursor: pointer;
     }
 `
-const Description = styled.div`
+const Description = styled.p`
     font-size: 0.8em !import;
     font-family: 'NanumSquareNeoBold';
     white-space: pre-line
--- src/conponents/CourseRow.tsx
+++ src/conponents/CourseRow.tsx
@@ -4,7 +4,16 @@ import styled from 'styled-components';
 const CourseRow: React.FC<Props> = (props) => {
     const { visiable, intro, steps } = props
     return (
-        <Container>
+        <Container visiable={visiable}>
+            <h3>{intro}</h3>
+            <StepList>
+                {steps.map(({id, title, desc}) => (
+                    <li key={id}>
+                        <p>{title}</p>
+                        <p>{desc}</p>
+                    </li>
+                ))}
+            </StepList>
         </Container>
     )
 }
@@ -16,7 +25,7 @@ type Props = {
     steps: Step[]
 }
 export type CourseData = {
-    id: number
+    id: string
     iconUrl: string
     title: string
     desc: string
@@ -24,13 +33,23 @@ export type CourseData = {
     steps: Step[]
 }
 export type Step = {
+    id: number
     title: string
     desc: string
-    period: string
 }
+const Container = styled.div<{visiable: boolean}>`
+    max-height: ${(props) => props.visiable ? "600px" : "0px"};
+    -webkit-transition: max-height 1s; 
+    -moz-transition: max-height 1s; 
+    -ms-transition: max-heigvht 1s; 
+    -o-transition: max-height 1s; 
+    transition: max-height 1s;
+    overflow: hidden;
+`
 
-const Container = styled.div`
-    
+const StepList = styled.ul`
+    list-style: none;
+    text-align: left;
 `
 
  
